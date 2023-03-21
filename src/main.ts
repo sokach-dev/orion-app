510345f1edd8b103dad6c94cca3a00b950f66330
sokach-dev <sokach-dev@hotmail.com>
2023-03-21 09:34:15 +0800

    remove vuex , add pinia

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import App from "./App.vue";
 import 'ant-design-vue/dist/antd.css';
 import Antd from 'ant-design-vue';
 import router from "./router";
-import store from './store';
+import { createPinia } from "pinia";
 
-createApp(App).use(store).use(router).use(Antd).mount("#app");
+const pinia = createPinia();
+
+createApp(App).use(pinia).use(router).use(Antd).mount("#app");
